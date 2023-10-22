cdda2d9897ed1eb5e42ca1be20d6ff285e89387f
Tiago Fernandes <tiagoferprojects@gmail.com>
2023-10-22 11:22:54 -0300

    Spring Boot Setup

---
+++ src/main/java/br/com/tiagofernandes/armada/ArmadaApplication.java
@@ -0,0 +1,13 @@
+package br.com.tiagofernandes.armada;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArmadaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArmadaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/tiagofernandes/armada/ArmadaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.tiagofernandes.armada;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArmadaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
