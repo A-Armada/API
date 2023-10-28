162b3095a8f93ed857efeda41dd9a4a447532e09
Tiago Fernandes <tiagoferprojects@gmail.com>
2023-10-28 20:10:11 -0300

    Defining the Return Route of Jobs

--- src/main/java/br/com/tiagofernandes/armada/job/JobController.java
+++ src/main/java/br/com/tiagofernandes/armada/job/JobController.java
@@ -1,8 +1,11 @@
 package br.com.tiagofernandes.armada.job;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,12 +16,19 @@ import org.springframework.web.bind.annotation.RestController;
 public class JobController {
 
   @Autowired
-  private IJobRepository officeRepository;
+  private IJobRepository jobRepository;
 
   @PostMapping("/create")
-  public ResponseEntity<?> create(@RequestBody JobModel jobModel) {
-    var jobCreated = this.officeRepository.save(jobModel);
+  public ResponseEntity<JobModel> create(@RequestBody JobModel jobModel) {
+    var jobCreated = this.jobRepository.save(jobModel);
 
     return ResponseEntity.status(HttpStatus.CREATED).body(jobCreated);
+  }
+
+  @GetMapping("/")
+  public ResponseEntity<List<JobModel>> list() {
+    var jobs = this.jobRepository.findAll();
+
+    return ResponseEntity.status(HttpStatus.OK).body(jobs);
   }
 }
