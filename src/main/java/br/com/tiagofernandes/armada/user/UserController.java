0918736ced3571f8d13a0912661d1faf5cf713f5
Tiago Fernandes <tiagoferprojects@gmail.com>
2023-10-30 21:14:16 -0300

    Defined User Acquisition Route Using Id

--- src/main/java/br/com/tiagofernandes/armada/user/UserController.java
+++ src/main/java/br/com/tiagofernandes/armada/user/UserController.java
@@ -1,8 +1,12 @@
 package br.com.tiagofernandes.armada.user;
 
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,5 +24,16 @@ public class UserController {
     var userCreated = this.userRepository.save(userModel);
 
     return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
+  }
+
+  @GetMapping("/{id}")
+  public ResponseEntity<?> getById(@PathVariable UUID id) {
+    var user = this.userRepository.findById(id).orElse(null);
+
+    if (user == null) {
+      return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário não encontrado!");
+    }
+
+    return ResponseEntity.status(HttpStatus.OK).body(user);
   }
 }
--- src/main/java/br/com/tiagofernandes/armada/user/UserModel.java
+++ src/main/java/br/com/tiagofernandes/armada/user/UserModel.java
@@ -5,13 +5,9 @@ import java.util.UUID;
 
 import org.hibernate.annotations.CreationTimestamp;
 
-import br.com.tiagofernandes.armada.job.JobModel;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
 import lombok.Data;
 
 @Data
