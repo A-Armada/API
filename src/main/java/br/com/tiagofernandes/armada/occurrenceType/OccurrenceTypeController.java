843b815c2fc6db25feb183be308a1f7c1356bc49
Tiago Fernandes <tiagoferprojects@gmail.com>
2023-11-08 20:32:10 -0300

    Create Get Occurrence Type by id method

--- src/main/java/br/com/tiagofernandes/armada/occurrenceType/OccurrenceTypeController.java
+++ src/main/java/br/com/tiagofernandes/armada/occurrenceType/OccurrenceTypeController.java
@@ -1,11 +1,13 @@
 package br.com.tiagofernandes.armada.occurrenceType;
 
 import java.util.List;
+import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -29,5 +31,16 @@ public class OccurrenceTypeController {
     var occurrenceTypes = this.occurrenceTypeRepository.findAll();
 
     return ResponseEntity.status(HttpStatus.OK).body(occurrenceTypes);
+  }
+
+  @GetMapping("/{id}")
+  public ResponseEntity<?> getById(@PathVariable UUID id) {
+    var occurrence = this.occurrenceTypeRepository.findById(id).orElse(null);
+
+    if (occurrence == null) {
+      return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário não encontrado!");
+    }
+
+    return ResponseEntity.status(HttpStatus.OK).body(occurrence);
   }
 }
